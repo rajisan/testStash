70ea4d3e4f816658715215423f5e8a0b8cd2187b
Raji Sankaran <rajisankaran14@gmail.com>
2015-07-11 10:50:03 -0400

    adding code to test stash commits

---
+++ src/com/ccur/testStash/StashCommit/StashCommit.java
@@ -0,0 +1,7 @@
+package com.ccur.testStash.StashCommit;
+
+public class StashCommit {
+public static void main(String[] args) {
+	System.out.println("TEst code to test automatic issue transmission");
+}
+}
