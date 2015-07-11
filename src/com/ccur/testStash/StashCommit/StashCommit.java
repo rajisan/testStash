bc24dfd051a8c0e5bbe6b5b0597a12a8f2dc5a88
Raji Sankaran <rajisankaran14@gmail.com>
2015-07-11 15:18:49 -0400

    POC-2 issue transition

--- src/com/ccur/testStash/StashCommit/StashCommit.java
+++ src/com/ccur/testStash/StashCommit/StashCommit.java
@@ -2,7 +2,7 @@ package com.ccur.testStash.StashCommit;
 
 public class StashCommit {
 public static void main(String[] args) {
-	System.out.println("TEst code to test automatic issue transition");
+	
 	System.out.println("Configuring JIRA to test automatic issue transition from stash");
 }
 }
