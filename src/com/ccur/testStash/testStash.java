92f77e1b05cb394e51f2de496e4e9fe141e8b961
Raji Sankaran <rajisankaran14@gmail.com>
2015-07-12 11:24:57 -0400

    POC-6 workflow

--- src/com/ccur/testStash/testStash.java
+++ src/com/ccur/testStash/testStash.java
@@ -4,6 +4,7 @@ public class testStash {
 public static void main(String[] args) {
 	
 	System.out.println("This is a file to test code commits with stash");
+	System.out.println("This is a file to full workflow with stash,jira and git");
 
 }
 }
