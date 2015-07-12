a991b9b9e3be7e3c49d20877b92cbe5e55f3ee92
Raji Sankaran <rajisankaran14@gmail.com>
2015-07-12 12:39:36 -0400

    POC-6 test workflow

--- src/com/ccur/testStash/testStash.java
+++ src/com/ccur/testStash/testStash.java
@@ -3,17 +3,8 @@ package com.ccur.testStash;
 public class testStash {
 public static void main(String[] args) {
 	
-<<<<<<< master
 	System.out.println("This is a file to test code commits with stash");
-=======
-	System.out.println("This is a file to test code commits with different branches in stash");
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
->>>>>>> local
+	System.out.println("This is a file to test code commits with full jira workflow");
+
 }
 }
