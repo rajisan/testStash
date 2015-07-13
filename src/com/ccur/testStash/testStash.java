6fdd59b34d59ff55300f9f34d101b51cdf4a66b5
Raji Sankaran <rajisankaran14@gmail.com>
2015-07-13 00:39:45 -0400

    POC-7 demo test

--- src/com/ccur/testStash/testStash.java
+++ src/com/ccur/testStash/testStash.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	
 	System.out.println("This is a file to test code commits with stash");
 	System.out.println("This is a file to test code commits with full jira workflow");
+	System.out.println("This is a file to demo full jira workflow");
 
 }
 }
